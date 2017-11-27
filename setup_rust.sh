a73c757fa90e213780355bf572953a8ba1890988
Mattias Petter Johansson <mpj@mpj.me>
2017-11-27 14:45:18 +0100

    setup_rust to postinstall

--- setup_rust.sh
+++ setup_rust.sh
@@ -1,6 +1,5 @@
 rustup update
-
 # install wasm package
 rustup target add wasm32-unknown-unknown --toolchain nightly
 # install wasm-gc (to make wasm file smaller)
-cargo install --git https://github.com/alexcrichton/wasm-gc
+cargo install --git https://github.com/alexcrichton/wasm-gc --force
